e4dc22fcc56b1de5168baf93b197c7bfb153783f
Megha Tagade <127646069+M1e2g3h4a5@users.noreply.github.com>
2026-07-20 18:37:53 +0530

    Create Performance_Analysis.sql

---
+++ Advance_Analytics/Performance_Analysis.sql
@@ -0,0 +1,13 @@
+/*
+  Comparing the current value to a target value
+  Helps measure success and compare performance
+
+  Current[Measure] - Target[Measure]
+
+Ex. Current Sales - Average Sales
+    Current year sales - previous year sales 
+    Current Sales - Lowest Sales
+*/
+
+-- 1) Analyze the yearly performance of products by comparing each products sales to both its average sales 
+-- performace and the previous years sales
